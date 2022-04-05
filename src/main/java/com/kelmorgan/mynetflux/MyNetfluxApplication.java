a03e4f7e63013f454fd9de76917ce23a42a588e7
kelmorgan <kelmorgan18@gmail.com>
2022-04-05 14:48:38 +0100

    First netflux commit

---
+++ src/main/java/com/kelmorgan/mynetflux/MyNetfluxApplication.java
@@ -0,0 +1,13 @@
+package com.kelmorgan.mynetflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNetfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNetfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelmorgan/mynetflux/MyNetfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelmorgan.mynetflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNetfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
